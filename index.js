96d7213bc2428e220a8ecdb3a7079a8417746739
HamdanAs <21hamdanas@gmail.com>
2023-03-22 14:55:38 +0700

    checking internet connection and alarm timeout

--- index.js
+++ index.js
@@ -5,26 +5,24 @@ const http = require('http')
 
 require('dotenv').config()
 
-let isOnline = false
-
-const checkInternetConnection = () => {
-    http.get("http://www.google.com/images/icons/product/chrome-48.png", res => {
-        isOnline = res.statusCode === 200
-
-        console.log(new Date().toLocaleString() + " : Koneksi Internet:", isOnline);
-    }).on('error', () => {
-        isOnline = false
-    })
-}
-
-checkInternetConnection()
-
-setInterval(checkInternetConnection, 1000 * 60)
-
 const SerialPort = serialport.SerialPort
 const mqttHost = process.env.MQTT_BROKER
 const mqttPort = process.env.MQTT_PORT
-const serial = process.env.PORT
+const serial = process.env.SERIAL_PORT
+const backendUrl = process.env.BACKEND_URL
+const serialNumber = process.env.SERIAL_NUMBER
+
+let isOnline = false
+let settings = {}
+let buzzerOff = true
+
+const getSetting = () => {
+    fetch(`${backendUrl}/public-node/setting/${serialNumber}`)
+        .then(res => res.json())
+        .then(res => settings = res.settings)
+}
+
+getSetting()
 
 const port = new SerialPort({ path: serial, baudRate: 9600 })
 const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
@@ -32,8 +30,57 @@ port.pipe(parser)
 
 port.write(`0,0,${isOnline ? 1 : 0},*`)
 
+const checkInternetConnection = () => {
+    http.get("http://www.google.com/images/icons/product/chrome-48.png", res => {
+        isOnline = res.statusCode === 200
+
+        port.write(`,,${isOnline ? 1 : 0},*`)
+
+        console.log(new Date().toLocaleString() + " : [NODEJS] Koneksi Internet:", isOnline);
+    }).on('error', err => {
+        isOnline = false
+
+        port.write(`,,0,*`)
+
+        console.log(new Date().toLocaleString() + " : [NODEJS] Koneksi Internet Error:", err);
+    })
+}
+
+checkInternetConnection()
+
+setInterval(checkInternetConnection, 1000 * 60)
+
 const mqttClient = mqtt.connect(`mqtt://${mqttHost}:${mqttPort}`)
-const mqttTopic = "EWS.telemetry"
+const telemetryTopic = "EWS.telemetry"
+const settingsTopic = "EWS.Settings." + serialNumber
+
+let buzzerTimeout
+
+const telemetryCallback = (response) => {
+    console.log(`${new Date().toLocaleString()} : [MQTT] Status Siaga: ${response.tma_level}, Status Buzzer: ${response.tma_level === 1 ? 1 : 0}, Status Internet: ${isOnline ? 1 : 0}`);
+
+    let command = `${response.tma_level},${(response.tma_level === 1 && buzzerOff) ? 1 : 0},1,*`
+
+    port.write(command)
+
+    buzzerOff = false;
+
+    buzzerTimeout = setTimeout(() => {
+        buzzerOff = true
+    }, settings.timer_alarm * 1000);
+}
+
+const settingsCallback = (response) => {
+    settings = response.settings
+
+    clearTimeout(buzzerTimeout)
+
+    buzzerOff = true
+
+    buzzerTimeout = setTimeout(() => {
+        buzzerOff = true
+    }, settings.timer_alarm * 1000);
+}
 
 parser.on('data', data => {
     console.log(new Date().toLocaleString() + " : Data received from arduino:", data);
@@ -49,36 +96,41 @@ port.on('close', () => {
 
 port.on('error', err => {
     console.log(new Date().toLocaleString() + " : [SERIAL PORT] Error:", err);
+
+    port.end()
 })
 
 mqttClient.on("connect", () => {
-    console.log(new Date().toLocaleString() + " : Connected to MQTT Broker");
-    mqttClient.subscribe(mqttTopic)
+    console.log(new Date().toLocaleString() + " : [MQTT] Connected to MQTT Broker");
+    mqttClient.subscribe([telemetryTopic, settingsTopic])
 })
 
 mqttClient.on('error', err => {
-    console.log(new Date().toLocaleString() + " : MQTT Error:", err);
+    console.log(new Date().toLocaleString() + " : [MQTT] Error:", err);
 })
 
 mqttClient.on('close', () => {
-    console.log(new Date().toLocaleString() + " : MQTT Closed");
+    console.log(new Date().toLocaleString() + " : [MQTT] Closed");
 })
 
 mqttClient.on('disconnect', () => {
-    console.log(new Date().toLocaleString() + " : MQTT Disconnected");
+    console.log(new Date().toLocaleString() + " : [MQTT] Disconnected");
 })
  
 mqttClient.on('reconnect', () => {
-    console.log(new Date().toLocaleString() + " : MQTT Reconnecting . . . ");
+    console.log(new Date().toLocaleString() + " : [MQTT] Reconnecting . . . ");
 })
 
 mqttClient.on('message', (topic, message) => {
-    console.log(`${new Date().toLocaleString()} : Message received on topic: ${topic}`);
+    console.log(`${new Date().toLocaleString()} : [MQTT] Message received on topic: ${topic}`);
 
-    let response = JSON.parse(message)
-    console.log(`Status Siaga: ${response.tma_level}, Status Buzzer: ${response.tma_level === 1 ? 1 : 0}, Status Internet: ${isOnline ? 1 : 0}`);
+    let response = JSON.parse(message.toString())
 
-    let command = `${response.tma_level},${response.tma_level === 1 ? 1 : 0},${isOnline ? 1 : 0},*`
+    if (topic === telemetryTopic) {
+        if (response.serial_number !== settings.iot_node) return
 
-    port.write(command)
+        telemetryCallback(response)
+    } else if (topic === settingsTopic) {
+        settingsCallback(response)
+    }
 })
