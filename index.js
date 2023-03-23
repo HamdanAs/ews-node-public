504a61d84e52f798b6a59d412ba934d91ada9557
HamdanAs <21hamdanas@gmail.com>
2023-03-23 09:30:04 +0700

    fix buzzer, indicator, and online statu

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const serialNumber = process.env.SERIAL_NUMBER
 let isOnline = false
 let settings = {}
 let buzzerOff = true
+let turnOnIndicator = 0
+let turnOnBuzzer = 0
 
 const getSetting = () => {
     fetch(`${backendUrl}/public-node/setting/${serialNumber}`)
@@ -34,13 +36,13 @@ const checkInternetConnection = () => {
     http.get("http://www.google.com/images/icons/product/chrome-48.png", res => {
         isOnline = res.statusCode === 200
 
-        port.write(`,,${isOnline ? 1 : 0},*`)
+        port.write(`${turnOnIndicator},${turnOnBuzzer},${isOnline ? 1 : 0},*`)
 
         console.log(new Date().toLocaleString() + " : [NODEJS] Koneksi Internet:", isOnline);
     }).on('error', err => {
         isOnline = false
 
-        port.write(`,,0,*`)
+        port.write(`0,0,0,*`)
 
         console.log(new Date().toLocaleString() + " : [NODEJS] Koneksi Internet Error:", err);
     })
@@ -60,14 +62,17 @@ let buzzerDelay
 const telemetryCallback = (response) => {
     console.log(`${new Date().toLocaleString()} : [MQTT] Status Siaga: ${response.tma_level}, Status Buzzer: ${response.tma_level === 1 ? 1 : 0}, Status Internet: ${isOnline ? 1 : 0}`);
 
-    let command = `${response.tma_level},${(response.tma_level === 1 && buzzerOff) ? 1 : 0},1,*`
+    turnOnIndicator = response.tma_level === 1 ? 3 : (response.tma_level === 3 ? 1 : (response.tma_level === 4 ? 0 : 2))
+    turnOnBuzzer = (response.tma_level === 1 && buzzerOff) ? 1 : 0
+
+    let command = `${turnOnIndicator},${turnOnBuzzer},1,*`
 
     port.write(command)
 
     buzzerOff = false;
 
     buzzerTimeout = setTimeout(() => {
-        let command = `${response.tma_level},0,1,*`
+        let command = `${turnOnIndicator},0,1,*`
 
         port.write(command)
     }, settings.timer_alarm * 1000);
@@ -134,9 +139,12 @@ mqttClient.on('message', (topic, message) => {
         if (response.serial_number !== settings.iot_node) return
 
         telemetryCallback(response)
+
+        mqttClient.publish(`connection.${serialNumber}`, JSON.stringify({response: "ok"}))
     } else if (topic === settingsTopic) {
         console.log(response);
 
         settingsCallback(response)
+        mqttClient.publish(`connection.${serialNumber}`, JSON.stringify({response: "ok"}))
     }
 })
